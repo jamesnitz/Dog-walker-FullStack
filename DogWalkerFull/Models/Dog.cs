c1491add35e73914a06ceabd50e6550c8c0e0a6a
jamesnitz <jamesanitz2@gmail.com>
2020-03-26 11:51:42 -0500

    first commit. FILE STRUCTURE MADE

---
+++ DogWalkerFull/Models/Dog.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DogWalkerFull.Models
+{
+    public class Dog
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int OwnerId { get; set; }
+
+        public string Breed { get; set; }
+
+        public string Notes { get; set; }
+        public Owner Owner { get; set; }
+
+
+    }
+}
---
+++ DogWalkerFull/Models/Walker.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DogWalkerFull.Models
+{
+    public class Walker
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int NeighborhoodId { get; set; }
+
+        public Neighborhood Neighborhood { get; set; }
+    }
+}
