5e77da8c49cfba61dfb66c83805014cbaf203966
jamesnitz <jamesanitz2@gmail.com>
2020-03-31 11:14:35 -0500

    added coumentation

--- DogWalkerFull/Models/Dog.cs
+++ DogWalkerFull/Models/Dog.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,10 @@ namespace DogWalkerFull.Models
     public class Dog
     {
         public int Id { get; set; }
-
+        [Required]
         public string Name { get; set; }
+        [Required]
+        [StringLength(40, MinimumLength = 2, ErrorMessage = "Dog Name must be between 2 and 40 characters")]
 
         public int OwnerId { get; set; }
 
